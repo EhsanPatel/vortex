ab22130cb63c7e68afaf43bb461feeebc7e9ac58
Ehsan Patel <ehsanpatel13@gmail.com>
2022-06-29 19:51:11 -0400

    Update vt.js

--- vt.js
+++ vt.js
@@ -2,7 +2,7 @@ window.onload = (event) => {
   fetch( 'https://vt.ehsanpatel.repl.co/api.php/SKU/VT-RZR-63601' )
     .then( response => response.json() )
     .then( response => {
-        document.querySelector('[data-type-vt="title"]').innerText = response.title;
+        document.querySelector('[data-type-vt="title"]').innerText = response.name;
         document.querySelector('[data-type-vt="sku"]').innerText = response.sku;
         document.querySelector('[data-type-vt="description"]').innerText = response.description;
     } );
