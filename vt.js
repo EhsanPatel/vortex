665a85a66bcfd1e925a67a98e20f069649b2d606
Ehsan Patel <ehsanpatel13@gmail.com>
2022-06-29 19:33:14 -0400

    Update vt.js

--- vt.js
+++ vt.js
@@ -1,5 +1,5 @@
 window.onload = (event) => {
-  document.querySelector('[data-type-vt="title"]').innerText = "This has been replaced";
-  document.querySelector('[data-type-vt="sku"]').innerText = "This has been replaced";
-  document.querySelector('[data-type-vt="description"]').innerText = "This has been replaced";
+  document.querySelector('[data-type-vt="title"]').innerText = "This has been replaced with the new title";
+  document.querySelector('[data-type-vt="sku"]').innerText = "This has been replaced with an SKU";
+  document.querySelector('[data-type-vt="description"]').innerText = "This has been replaced with the description";
 };
